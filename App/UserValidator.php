1ee0c26f955e731b5b08089964d2e42f2d1e0aa0
k-graczyk <kamil.graczyk1996@wp.pl>
2024-09-12 09:42:26 +0200

    UserValidator v_1 with single regex

--- App/UserValidator.php
+++ App/UserValidator.php
@@ -6,5 +6,24 @@ namespace App;
 
 class UserValidator
 {
+    public function validateEmail(string $email): bool
+    {
+        $email = $this->sanitizeInput($email);
 
+        $pattern = "/^[a-z\d]+[\w.-]*@(?:[a-z\d]+(?:-[a-z\d]+)*\.){1,5}[a-z]{2,6}$/i";
+
+        return preg_match($pattern, $email) === 1;
+    }
+
+    public function validatePassword(string $password): bool
+    {
+        $password = $this->sanitizeInput($password);
+        $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])(?!.*\s).{8,}$/';
+        return preg_match($pattern, $password) === 1;
+    }
+
+    private function sanitizeInput(string $input): string
+    {
+        return filter_var(trim($input), FILTER_SANITIZE_SPECIAL_CHARS);
+    }
 }
