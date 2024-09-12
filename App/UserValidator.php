19df51ad2e1384bd828985a9192047012aecd8a6
k-graczyk <kamil.graczyk1996@wp.pl>
2024-09-12 09:45:22 +0200

    v_2 with errorBag and throw exception

--- App/UserValidator.php
+++ App/UserValidator.php
@@ -17,10 +17,36 @@ class UserValidator
 
     public function validatePassword(string $password): bool
     {
-        $password = $this->sanitizeInput($password);
-        $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])(?!.*\s).{8,}$/';
-        return preg_match($pattern, $password) === 1;
+        $errors = [];
+        if (!preg_match('/[a-z]/', $password)) {
+            array_push($errors, "Hasło musi zawierać małą literę.");
+        }
+        if (!preg_match('/[A-Z]/', $password)) {
+            array_push($errors, "Hasło musi zawierać dużą literę.");
+        }
+
+        if (!preg_match('/\d/', $password)) {
+            array_push($errors, "Hasło musi zawierać cyfrę.");
+        }
+
+        if (!preg_match('/[\W_]/', $password)) {
+            array_push($errors, "Hasło musi zawierać znak specjalny");
+        }
+
+        if (preg_match('/\s/', $password)) {
+            array_push($errors, "Hasło nie może zawierać spacji");
+        }
+        if (strlen($password) < 8) {
+            array_push($errors, "Hasło musi mieć co najmniej 8 znaków.");
+        }
+        if (!empty($errors)) {
+            $errorMessage = implode("\n", $errors);
+            throw new \Exception($errorMessage);
+        }
+
+        return true;
     }
+
 
     private function sanitizeInput(string $input): string
     {
